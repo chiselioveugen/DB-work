05dd8d41355ddf0ed4d3b1ff792588e82ed3970d
Chiseliov Eugen-Catalin <chiseliov.eugen@mail.ru>
2018-11-28 00:19:01 +0200

    formatting

--- lab8/sql/task6.sql
+++ lab8/sql/task6.sql
@@ -1,8 +1,8 @@
 drop table if exists graph
 CREATE TABLE graph (
-		Id_nr int PRIMARY KEY,
-		dependent_nr int
-		);
+	Id_nr int PRIMARY KEY,
+	dependent_nr int
+);
 
 INSERT INTO graph VALUES
 (5,0), (4,2), (3,2), (1,0), (2,1), (0, null);
@@ -10,13 +10,11 @@ INSERT INTO graph VALUES
 select * from graph;
 
 WITH graph_cte AS (
-		SELECT Id_nr , dependent_nr FROM graph
-		WHERE Id_nr = 3 and dependent_nr = 2
-		
-		UNION ALL
-		
-		SELECT graph.Id_nr, graph.dependent_nr FROM graph
-		INNER JOIN graph_cte
-		ON graph.ID_nr = graph_cte.dependent_nr
+	SELECT Id_nr , dependent_nr FROM graph
+	WHERE Id_nr = 3 and dependent_nr = 2
+	UNION ALL
+	SELECT graph.Id_nr, graph.dependent_nr FROM graph
+	INNER JOIN graph_cte
+	ON graph.ID_nr = graph_cte.dependent_nr
 )
 SELECT * from graph_cte
