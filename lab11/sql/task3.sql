25a1ed6fc93691ff6d27751335a44d40387d4cb0
Chiseliov Eugen-Catalin <chiseliov.eugen@mail.ru>
2018-12-11 22:32:30 +0200

    LAB11 update

--- lab11/sql/task3.sql
+++ lab11/sql/task3.sql
@@ -1,17 +1,5 @@
-RESTORE DATABASE univer_restore
-FROM DISK = 'D:\UNIVERSITATE\DB-work\lab11\files\device1_task1.bak'
-WITH MOVE 'universitatea' TO 'D:\UNIVERSITATE\DB\Data\universitatea_f1',
-MOVE 'universitatea_File2' TO 'D:\UNIVERSITATE\DB\Data\universitatea_f2',
-MOVE 'universitatea_File3' TO 'D:\UNIVERSITATE\DB\Data\universitatea_f3',
-MOVE 'universitatea_log' TO 'D:\UNIVERSITATE\DB\SQL-logs\universitatea_f4.ldf',
-NORECOVERY
+EXEC sp_addumpdevice 'DISK','backup_Log', 'D:\UNIVERSITATE\DB-work\lab11\files\task3.bak'
 GO
-
-RESTORE DATABASE univer_restore
-FROM DISK = 'D:\UNIVERSITATE\DB-work\lab11\files\device2_task2.bak'
-WITH NORECOVERY
+BACKUP LOG universitatea
+TO backup_Log
 GO
-
-RESTORE LOG univer_restore
-FROM DISK = 'D:\UNIVERSITATE\DB-work\lab11\files\task3.bak'
-WITH NORECOVERY
